49707df4cfe12ce7b08f4d8d7d03a3b7bb57c18c
mtalli <mauno.talli@hotmail.com>
2023-07-25 02:00:11 +0300

    Blank html doc

---
+++ rock-paper-scissors.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <title>Blank HTML document with a script tag</title>
+</head>
+
+<body>
+
+    
+
+    <script>
+        
+
+
+    </script>
+
+
+
+</body>
+</html>
+
+
+
